30408daaef529e92e1ab2ac23af86cbeef31367f
bakawoot <agodaniel1@gmail.com>
2020-06-28 11:14:03 +0200

    The packages we need.

---
+++ setup.sh
@@ -0,0 +1,29 @@
+echo
+echo "== https://github.com/bakawoot/PVPGN-setup =="
+echo
+
+echo "-- Installing packages. --"
+
+echo "-- Build-essential --"
+apt-get install build-essential 
+
+echo "-- Clang --"
+apt-get install clang
+
+echo "-- LibC++-Dev --"
+apt-get install libc++-dev
+
+echo "-- Git --"
+apt-get install git
+
+echo "-- Cmake --"
+apt-get install cmake
+
+echo "-- ZLib1G-DEV --"
+apt-get install zlib1g-dev
+
+echo "-- LibLua5.1-0-DEV --"
+apt-get install liblua5.1-0-dev 
+
+echo "-- LibMYSQL++-DEV --"
+apt-get install libmysql++-dev
