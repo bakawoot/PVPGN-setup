333a9eea574d91ccd6254b8bee98f324ad0f56ce
bakawoot <agodaniel1@gmail.com>
2020-06-28 11:34:53 +0200

    Installing procedure

--- setup.sh
+++ setup.sh
@@ -35,8 +35,20 @@ echo
 echo "- LibMYSQL++-DEV"
 apt-get install libmysql++-dev
 
-read -r -p "'Master' or 'Develop' branch: " branch
+read -r -p "'master' or 'develop' branch: " branch
 
 echo
 echo "-- Cloning the latest PVPGN files --"
 git clone --depth=50 --branch="${branch}" https://github.com/pvpgn/pvpgn-server.git pvpgn/pvpgn-server
+
+mv pvpgn/pvpgn-server pvpgn/pvpgn-source
+mkdir pvpgn/pvpgn-source/build
+
+echo
+echo "-- CMake --"
+cd pvpgn/pvpgn-source/build
+cmake -D CMAKE_INSTALL_PREFIX=/usr/local/pvpgn -D WITH_MYSQL=true -D WITH_LUA=true ../
+
+echo
+echo "-- Make & Install --"
+make && make install
