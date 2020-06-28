fdf7e093848d1df5e86e545995706a11ccc5d525
bakawoot <agodaniel1@gmail.com>
2020-06-28 11:25:36 +0200

    Can we clone git like that?

--- setup.sh
+++ setup.sh
@@ -2,28 +2,41 @@ echo
 echo "== https://github.com/bakawoot/PVPGN-setup =="
 echo
 
-echo "-- Installing packages. --"
+echo "-- Installing packages --"
 
-echo "-- Build-essential --"
+echo "- Build-essential"
 apt-get install build-essential 
 
-echo "-- Clang --"
+echo
+echo "- Clang"
 apt-get install clang
 
-echo "-- LibC++-Dev --"
+echo
+echo "- LibC++-Dev"
 apt-get install libc++-dev
 
-echo "-- Git --"
+echo
+echo "- Git"
 apt-get install git
 
-echo "-- Cmake --"
+echo
+echo "- Cmake"
 apt-get install cmake
 
-echo "-- ZLib1G-DEV --"
+echo
+echo "- ZLib1G-DEV"
 apt-get install zlib1g-dev
 
-echo "-- LibLua5.1-0-DEV --"
+echo
+echo "- LibLua5.1-0-DEV"
 apt-get install liblua5.1-0-dev 
 
-echo "-- LibMYSQL++-DEV --"
+echo
+echo "- LibMYSQL++-DEV"
 apt-get install libmysql++-dev
+
+read -r -p "'Master' or 'Develop' branch: " branch
+
+echo
+echo "-- Cloning the latest PVPGN files --"
+git clone --depth=50 --branch="${branch}" https://github.com/pvpgn/pvpgn-server.git pvpgn/pvpgn-server
