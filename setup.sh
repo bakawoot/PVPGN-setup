8ff32da6796f5bbd44b4e5287d5cf9d05373005d
bakawoot <agodaniel1@gmail.com>
2020-06-28 17:16:05 +0200

    DL packages for D2GS & WINE

--- setup.sh
+++ setup.sh
@@ -87,6 +87,17 @@ read -r -p "'yes' or 'no': " d2gsSelector
 
 if  [ "$d2gsSelector" = "yes" ]; then
     echo
+    echo "-- Installing additional packages --"
+    apt-get install unrar
+    apt-get install lib32zl
+    apt-get install gcc-multilib
+    apt-get install xserver-xorg-dev:i386
+    apt-get install libfreetype6-dev:i386
+    dpkg --add-architecture i386
+
+    cd ..
+    cd ..
+    echo
     echo "-- Do you want D2GS?"
     echo "Which D2GS version?"
     echo "[1]1.09d"
@@ -100,24 +111,38 @@ if  [ "$d2gsSelector" = "yes" ]; then
     read -r -p "'[1/2/3/4/5/6/7]': " d2gsVersionSelector
     case "$d2gsVersionSelector" in
         #case 1
-        "1") wget http://cdn.pvpgn.pro/d2gs/D2GS-109d.zip ;;
+        "1") wget http://cdn.pvpgn.pro/d2gs/D2GS-109d.zip
+        unrar e D2GS-109d.zip d2gs/
+        ;;
 
         #case 2
-        "2") wget http://cdn.pvpgn.pro/d2gs/D2GS-110-bin-beta6.rar ;;
+        "2") wget http://cdn.pvpgn.pro/d2gs/D2GS-110-bin-beta6.rar
+        unrar e D2GS-110-bin-beta6.rar d2gs/
+        ;;
         
         #case 3
-        "3") wget http://cdn.pvpgn.pro/d2gs/D2GS-111b-build46.rar ;;
+        "3") wget http://cdn.pvpgn.pro/d2gs/D2GS-111b-build46.rar
+        unrar e D2GS-111b-build46.rar d2gs/
+        ;;
         
         #case 4
-        "4") wget hhttp://cdn.pvpgn.pro/d2gs/D2GS-112a-build01.rar ;;
+        "4") wget hhttp://cdn.pvpgn.pro/d2gs/D2GS-112a-build01.rar
+        unrar e D2GS-112a-build01.rar d2gs/
+        ;;
         
         #case 5
-        "5") wget http://cdn.pvpgn.pro/d2gs/D2GS-113-build02.rar ;;
+        "5") wget http://cdn.pvpgn.pro/d2gs/D2GS-113-build02.rar
+        unrar e D2GS-113-build02.rar d2gs/
+        ;;
         
         #case 6
-        "6") wget http://cdn.pvpgn.pro/d2gs/D2GS-113c-build03.rar ;;
+        "6") wget http://cdn.pvpgn.pro/d2gs/D2GS-113c-build03.rar
+        unrar e D2GS-113c-build03.rar d2gs/
+        ;;
         
         #case 7
-        "7") wget http://cdn.pvpgn.pro/d2gs/D2GS-113d-build02_mxcen.rar ;;
+        "7") wget http://cdn.pvpgn.pro/d2gs/D2GS-113d-build02_mxcen.rar
+        unrar e D2GS-113d-build02_mxcen.rar d2gs/
+        ;;
     esac
 fi
