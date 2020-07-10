8ea70fe7790c7d58cb7c92d2c627125b7f9ff0a7
bakawoot <agodaniel1@gmail.com>
2020-07-10 20:18:39 +0200

    Delete wine-5.2.tar.xz file after unpacking.

--- setup.sh
+++ setup.sh
@@ -137,6 +137,7 @@ if  [ "$d2gsSelector" = "yes" ]; then
     cd /pvpgn/wine/
     wget https://dl.winehq.org/wine/source/5.x/wine-5.2.tar.xz
     tar xf wine-5.2.tar.xz
+    rm wine-5.2.tar.xz
     cd /pvpgn/wine/wine-5.2/server
     rm sock.c
     wget https://raw.githubusercontent.com/bakawoot/PVPGN-setup/master/Sock%20files/5.2/sock.c
